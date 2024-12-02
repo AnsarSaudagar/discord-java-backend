99f9f345b98c2ed6b260a27d043d29fd7bb373f9
ansar <ansarsaudagar40@gmail.com>
2024-12-03 00:42:41 +0530

    basic code refractoring

--- src/main/java/com/example/discord/repositories/FriendRepository.java
+++ src/main/java/com/example/discord/repositories/FriendRepository.java
@@ -7,13 +7,4 @@ import java.util.List;
 
 @Repository
 public interface FriendRepository extends JpaRepository<Friend, Long> {
-
-//    // Find friendships for a specific user
-//    List<Friend> findByUser1UserIdOrUser2UserId(Long userId1, Long userId2);
-//
-//    // Find specific friendship between two users
-//    Friend findByUser1UserIdAndUser2UserId(Long userId1, Long userId2);
-//
-//    // Find pending friendship requests for a user
-//    List<Friend> findByUser2UserIdAndStatus(Long userId, Friend.FriendshipStatus status);
 }
