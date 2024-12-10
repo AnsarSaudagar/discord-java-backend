5c869abe316f7826564505a7ec9d5fcb1917f0c7
ansar <ansarsaudagar40@gmail.com>
2024-12-11 01:09:34 +0530

    removed cache code

--- src/main/java/com/example/discord/config/ApplicationConfiguration.java
+++ src/main/java/com/example/discord/config/ApplicationConfiguration.java
@@ -1,18 +1,8 @@
 package com.example.discord.config;
 
 import com.example.discord.repositories.UserRepository;
-
-import java.time.Duration;
-
-import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -58,18 +48,6 @@ public class ApplicationConfiguration {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
-    }
-
-    @Bean
-    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
-        RedisCacheConfiguration cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
-                .entryTtl(Duration.ofHours(1)) 
-                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
-                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
-
-        return RedisCacheManager.builder(redisConnectionFactory)
-                .cacheDefaults(cacheConfig)
-                .build();
     }
     
 }
--- src/main/java/com/example/discord/services/AuthenticationService.java
+++ src/main/java/com/example/discord/services/AuthenticationService.java
@@ -5,7 +5,6 @@ import com.example.discord.dtos.RegisterUserDto;
 import com.example.discord.entity.User;
 import com.example.discord.repositories.UserRepository;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,9 +12,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationService {
-
-    @Autowired
-    private CacheService cacheService;
 
     private final UserRepository userRepository;
 
@@ -45,8 +41,6 @@ public class AuthenticationService {
             data.getDob()
         );
         User savedUser = userRepository.save(user);
-        // cacheService.setValue("ansar", "hussain");
-        System.out.println("cache = " + cacheService.getValue("ansar"));
         return savedUser;
     }
 
